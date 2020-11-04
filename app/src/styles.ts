80d363a5f502cae26097f9f208ba2185ae274bb7
Josh Medeski <josh.medeski@gmail.com>
2020-11-03 22:08:33 -0600

    Add styled components, remove default styling

---
+++ app/src/styles.ts
@@ -0,0 +1,72 @@
+export enum MaxWidth {
+  thin = "maxWidth: 200px",
+  wide = "maxWidth: 800px",
+  full = "maxWidth: 100%",
+}
+
+export enum FontFamily {
+  system = "font-family: -apple-system, Segoe UI, Roboto, Noto Sans, Ubuntu, Cantarell, Helvetica Neue;",
+}
+
+export enum FontSize {
+  smallest = "fontSize: 12px;",
+  small = "fontSize: 14px;",
+  default = "fontSize: 16px;",
+  large = "fontSize: 18px;",
+  larger = "fontSize: 20px;",
+  largerX2 = "fontSize: 24px;",
+  largerX3 = "fontSize: 36px;",
+  largerX4 = "fontSize: 48px;",
+  largerX5 = "fontSize: 60px;",
+  largest = "fontSize: 72px;",
+}
+
+export enum Spacing {
+  less = "12px",
+  default = "24px",
+  more = "30px",
+}
+
+export enum FontWeight {
+  normal = "fontWeight: normal",
+  bold = "fontWeight: 800",
+  bolder = "fontWeight: 900",
+}
+
+export enum ErrorColor {
+  lighter = "color: #",
+  default = "color: #",
+  darker = "color: #",
+}
+
+export enum SuccessColor {
+  lighter = "color: #",
+  default = "color: #",
+  darker = "color: #",
+}
+
+export enum WarningColor {
+  lighter = "color: #",
+  default = "color: #",
+  darker = "color: #",
+}
+
+export enum BorderRadius {
+  less = "border-radius: 3px",
+  default = "border-radius: 5px",
+  more = "border-radius: 7px",
+}
+
+export enum TextColor {
+  inverted = "color: #ffffff",
+  lightest = "color: #",
+  lighter = "color: #",
+  light = "color: #",
+  default = "color: #",
+}
+
+export enum BackgroundColor {
+  inverted = "backgroundColor: #000",
+  default = "backgroundColor: #fff",
+  dark = "backgroundColor: #fff",
+}
